c04aac4463c9471596a9037a4709c890df6b36ae
Vitor Martinez <vitormartinez751@gmail.com>
2020-10-21 21:28:51 -0300

    Adição de 2 novos dialogs

---
+++ src/front/controllers/MessageDialogController.java
@@ -0,0 +1,19 @@
+package front.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.text.Text;
+
+public class MessageDialogController {
+    public JFXButton btnClose;
+    public JFXButton btnOk;
+    public Text txtMessage;
+    public Text txtHeader;
+
+    public void btnClose_Click(){
+
+    }
+
+    public void btnOk_Click(){
+
+    }
+}
---
+++ src/front/controllers/YesOrNoDialogController.java
@@ -0,0 +1,19 @@
+package front.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.text.Text;
+
+public class YesOrNoDialogController {
+    public Text txtMessage;
+    public Text txtHeader;
+    public JFXButton btnClose;
+    public JFXButton btnNo;
+    public JFXButton btnYes;
+
+    public void btnClose_Click(){
+
+    }
+    public void btnYes_Click(){
+
+    }
+}
