e9b289666b5fd936aea7632a9b2f690ce9dd3dc2
Matheus Carvalho <matheuscarva@hotmail.com>
2020-10-21 21:21:14 -0300

    implementado login e hash nas senhas

--- src/front/controllers/LoginController.java
+++ src/front/controllers/LoginController.java
@@ -1,6 +1,10 @@
 package front.controllers;
 
+import business.UserBusiness;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import common.VO.User;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,24 +19,36 @@ public class LoginController {
     public JFXButton btnClose;
     public JFXButton btnLogin;
     public JFXButton btnRegister;
+    public JFXPasswordField txtPassword;
+    public JFXTextField txtEmail;
     public AnchorPane apLogin;
-    public void btnClose_Click(){
+
+    public void btnClose_Click() {
         System.exit(0);
     }
+
     public void btnLogin_Click() throws IOException {
-        Stage LoginStage = (Stage)btnLogin.getScene().getWindow();
-        LoginStage.close();
-        Parent root = FXMLLoader.load(getClass().getResource("../views/MainWindow.fxml"));
-        Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
-        Stage primaryStage = new Stage();
-        primaryStage.setTitle("Inicio");
-        primaryStage.setScene(scene);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.show();
+
+        UserBusiness Ub = new UserBusiness();
+        User user = Ub.login(txtEmail.getText(), txtPassword.getText());
+        if (user != null) {
+            Stage LoginStage = (Stage) btnLogin.getScene().getWindow();
+            LoginStage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("../views/MainWindow.fxml"));
+            Scene scene = new Scene(root);
+            scene.setFill(Color.TRANSPARENT);
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("Inicio");
+            primaryStage.setScene(scene);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            primaryStage.show();
+        } else {
+
+        }
     }
+
     public void btnRegister_Click() throws IOException {
-        Stage LoginStage = (Stage)btnLogin.getScene().getWindow();
+        Stage LoginStage = (Stage) btnLogin.getScene().getWindow();
         LoginStage.close();
         Parent root = FXMLLoader.load(getClass().getResource("../views/RegisterWindow.fxml"));
         Scene scene = new Scene(root);
